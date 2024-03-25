98d23a9e85f62e6ac5aa00acb220aae5cf164af0
lilxoli <dornfeldz@gmail.com>
2024-03-25 18:46:07 +0100

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
         <div>
             <div className="mt-16 px-10 py-6 w-full bg-black h-48">
                 <div className="grid grid-cols-2 gap-6">
-                    <h2 className="flex items-center gap-2 text-5xl font-bold"><span className="material-symbols-outlined text-orange-400 text-4xl">paid</span><span className="text-orange-400">Crypto</span>Pulse</h2>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 font-medium">
+                    <h2 className="flex items-center gap-2 text-xl lg:text-5xl font-bold "><span className="material-symbols-outlined text-orange-400 text-xl lg:text-4xl">paid</span><span className="text-orange-400">Crypto</span>Pulse</h2>
+                    <div className="grid grid-cols-2 sm:grid-cols-3 font-medium text-xs lg:text-lg">
                         <Link className="mb-6" to="/">Home</Link>
                         <Link to="https://www.coinlore.com/cryptocurrency-data-api" target="_blank">API</Link>
                         <Link className="mb-6" to="https://dornfeld.dev" target="_blank">Contact</Link>
