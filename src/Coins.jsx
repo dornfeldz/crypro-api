e443b19967c3c41a0748fd4fc6e84ef30eecb03a
lilxoli <dornfeldz@gmail.com>
2024-03-25 18:56:16 +0100

    Update Coins.jsx

--- src/Coins.jsx
+++ src/Coins.jsx
@@ -36,7 +36,7 @@ function Coins() {
           {coins.map(coin =>(
               <tr className="hover:bg-black border-b border-b-neutral-500 h-12" key={coin.symbol}>
                 <td className="px-2">{coin.rank}</td>
-                <td className="flex flex-col lg:flex-row lg:gap-2 lg:items-center pt-1"><span  className="text-md lg:text-2xl font-semibold">{coin.name}</span> <span className="text-orange-400 text-xs lg:text-sm">({coin.symbol})</span></td>
+                <td className="flex flex-col lg:flex-row lg:gap-2 lg:items-center pt-2"><span  className="text-md lg:text-2xl font-semibold items-center">{coin.name}</span> <span className="text-orange-400 text-xs lg:text-sm">({coin.symbol})</span></td>
                 <td className="px-2">${coin.price_usd}</td>
                 <td className="px-2">{coin.percent_change_1h}</td>
                 <td className="px-2">{coin.percent_change_24h}</td>
